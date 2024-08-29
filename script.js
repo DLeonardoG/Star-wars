0bf2b37c5926075d15a062b1b5ddafe248dc8bad
DLeonardoG <darkleonardog@gmail.com>
2024-08-29 17:08:21 -0500

    feat: :lipstick: Se agregan subdiviciones para su correcto funcionamiento
    Se agregan subdiviciones para su correcto funcionamiento de las consultas con los eventos y las llamadas correspondientes a la API de Star Wars
    
    By @DLeonardoG

--- script.js
+++ script.js
@@ -58,50 +58,86 @@ async function busqueda_personajes() {
         contenedorElemento.append(div);
     })
 }
-busqueda_personajes()
-hacerConsultasBarra("peliculas");
-agregarEventoConsultas()
+
 botonesCategorias.forEach(boton => {
     boton.addEventListener("click", (e) => {
         botonesCategorias.forEach(boton => boton.classList.remove("active"));
         e.currentTarget.classList.add("active");
-        if (e.currentTarget.id != "peliculas") {
-            hacerConsultasBarra(e.currentTarget.id);
-            agregarEventoConsultas()
-            // AQUI SE MUESTRA LA CONSULTA DE TODOS LOS ELEMENTOS DE ESTE ITEM EN ESPECIFICO MAMAHUEVO
-        } else {
-                hacerConsultasBarra(e.currentTarget.id);
+        const per = e.currentTarget.id;
+        switch (per) {
+            case "personajes":
+                busqueda_personajes();
+                console.log ("Bandera de gay")
+                const consultasPersonajes = ["Color de ojos", "Color de piel","Genero","Color de pelo"];
+                hacerConsultasBarra (consultasPersonajes)
                 agregarEventoConsultas()
-                // AQUI TODOS LOS DE PELICULAS DE ESTE ITEM EN ESPECIFICO MMH
-            }
+                break;
+            case "peliculas":
+                // AQUI SE HACE LA CONSULTA DE PELICULAS
+                console.log("Consulta de peliculas");
+                const consultasPeliculas = ["Color de ojos", "Color de piel","Genero","Color de pelo"];
+                hacerConsultasBarra (consultasPeliculas)
+                agregarEventoConsultas()
+                break;
+            case "naves":
+                // AQUI SE HACE LA CONSULTA DE NAVES
+                console.log("Consulta de naves");
+                const consultasNaves = ["Color de ojos", "Color de piel","Genero","Color de pelo"];
+                hacerConsultasBarra (consultasNaves)
+                agregarEventoConsultas()
+                break;
+            case "especies":
+                // AQUI SE HACE LA CONSULTA DE ESPECIES
+                console.log("Consulta de especies");
+                const consultasEspecies = ["Color de ojos", "Color de piel","Genero","Color de pelo"];
+                hacerConsultasBarra (consultasEspecies)
+                agregarEventoConsultas()
+                break;
+            case "vehiculos":
+                // AQUI SE HACE LA CONSULTA DE VEHICULOS
+                console.log("Consulta de vehículos");
+                const consultasVehiculos = ["Color de ojos", "Color de piel","Genero","Color de pelo"];
+                hacerConsultasBarra (consultasVehiculos)
+                agregarEventoConsultas()
+                break;
+            default:
+                console.log("Error en la categoría seleccionada");
+        }
+    });
+});
+function agregarEventoConsultas () {
+    const botonesCategoriasConsulta = document.querySelectorAll(".boton-categoria-consulta");
+    botonesCategoriasConsulta.forEach(boton => {
+        boton.addEventListener("click", (e) => {
+            botonesCategoriasConsulta.forEach(boton => boton.classList.remove("active-consulta"));
+            e.currentTarget.classList.add("active-consulta");
+            console.log( e.currentTarget)
+            // DE AQUI PARA ABAJO SE HACEN LAS CONSULTAS POR EL SUB ELEMENTO DE LA SUBLISTA
+            // const prueba = document.createElement("div");
+            // prueba.classList.add("elemento");
+            // prueba.innerHTML = ""
+            // prueba.innerHTML =  `
+            // <h2>${e.currentTarget.id}</h2>`
+            // console.log(prueba)
+            // contenedorElemento.prepend(prueba)
+            contenedorElemento.innerHTML = "";
+            busqueda_personajes()
         });
-    });
-    function agregarEventoConsultas () {
-        const botonesCategoriasConsulta = document.querySelectorAll(".boton-categoria-consulta");
-        botonesCategoriasConsulta.forEach(boton => {
-            boton.addEventListener("click", (e) => {
-                botonesCategoriasConsulta.forEach(boton => boton.classList.remove("active-consulta"));
-                e.currentTarget.classList.add("active-consulta");
-                console.log( e.currentTarget)
-
-                // DE AQUI PARA ABAJO SE HACEN LAS CONSULTAS POR EL SUB ELEMENTO DE LA SUBLISTA
-                const prueba = document.createElement("div");
-                prueba.classList.add("elemento");
-                prueba.innerHTML = ""
-                prueba.innerHTML =  `
-                <h2>${e.currentTarget.id}</h2>`
-                console.log(prueba)
-                contenedorElemento.prepend(prueba)
-            });
-    });
-    }
+});
+}
 // ESTA FUNCION HACE LAS SUBCONSULTAS DE LA PAGINA DE LA 1 A LA 4 PARA PODER REALIZAR LAS ADECUADAS, HAY QUE PERSONALIZARLA YA SEA CON UN ARRAY Y SE LE HACE FOREACH O ALGO ASI PARA PODER REALIZARLA AUTOMATICA
-function hacerConsultasBarra (parametro){
+function hacerConsultasBarra (array){
     consultasBarra.innerHTML = ``
-    for (let i = 1; i < 5; i++) {
+    array.forEach((elemento) => {
         consultasBarra.innerHTML += `
         <li>
-            <button id="${parametro + i}" class="boton-menu-consulta boton-categoria-consulta"><span>${parametro + " " + i}</span></button>
+            <button id="${elemento}" class="boton-menu-consulta boton-categoria-consulta"><span>${elemento}</span></button>
         </li>`
-    }
+    })
 }
+
+busqueda_personajes()
+const consultasPeliculas = ["Color de ojos", "Color de piel","Genero","Color de pelo"];
+hacerConsultasBarra(consultasPeliculas);
+agregarEventoConsultas()
+console.log(consultasPeliculas)
